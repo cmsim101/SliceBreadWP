11405a94a0bf494ea0bfe25ccba7d213860798d1
Chris Simpson <chrissimpson@Mac-2.broadband>
2025-07-23 17:05:59 +0100

    Initial commit: Astra child theme and .gitignore

---
+++ wp-content/themes/astra-child/functions.php
@@ -0,0 +1,5 @@
+<?php
+function astra_child_enqueue_styles() {
+    wp_enqueue_style( 'astra-parent-style', get_template_directory_uri() . '/style.css' );
+}
+add_action( 'wp_enqueue_scripts', 'astra_child_enqueue_styles' );
